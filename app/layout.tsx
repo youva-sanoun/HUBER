fe8ec961bb5af6c8ea8d604270fc6e583599cb1f
youva-sanoun <sanounboussadjuba@gmail.com>
2025-01-09 02:43:03 +0100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,31 @@
-import { Inter } from 'next/font/google'
-import "./globals.css"
-import { Navigation } from "@/components/navigation"
-import { ThemeProvider } from "@/components/theme-provider"
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Navigation } from "@/components/navigation";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-background`}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <div className="min-h-screen flex">
             <Navigation />
-            <div className="flex-1 ml-64">              
+            <div className="flex-1 ml-64">
               <main className="max-w-[1800px] mx-auto px-6 py-8">
                 {children}
               </main>
             </div>
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
-
